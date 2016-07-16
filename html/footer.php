43c2c35734405cb31566718855f51c42a14dc982
tuancuongth88 <tuancuongth88@gmail.com>
2016-07-16 15:50:09 +0700

    html

---
+++ html/footer.php
@@ -0,0 +1,177 @@
+<footer>
+			<div class="hh-container">
+				<div class="main-width">
+					<div class="left">
+						<div class="hotro">
+							<div class="block">
+								<div class="head">
+									<h3><i class="fa fa-question-circle"></i> Hỗ trợ - Dịch vụ</h3>
+								</div>
+								<div class="content">
+									<ul>
+										<li><strong>Tổng đài hỗ trợ:</strong> <b>19002091</b></li>
+										<li class="padding">Hà Nội: <b>(04).73045566</b></li>
+										<li class="padding">TP.HCM: <b>(08).73045566</b></li>
+									</ul>
+									<ul>
+										<li><strong>Phản ánh góp ý khiếu nại dịch vụ</strong></li>
+										<li class="padding">hotline : <b>19002091 nhánh số 0</b></li>
+										<li class="padding">email: <a href="mailto:cskh@hoanghamobile.com">cskh@hoanghamobile.com</a></li>
+									</ul>
+									<ul>
+										<li><strong>Bán hàng khối doanh nghiệp</strong></li>
+										<li class="padding">email: <a href="mailto:kinhdoanh@hoanghamobile.com">kinhdoanh@hoanghamobile.com</a></li>
+										<li class="padding">Hotline: <b>0932.32.28.48</b></li>
+									</ul>
+									<ul class="bullet">
+										<li><a>Hỗ trợ - Dịch vụ</a></li>
+										<li><a href="https://hoanghamobile.com/huong-dan-mua-hang">Hướng dẫn mua hàng</a></li>
+										<li><a href="https://hoanghamobile.com/chinh-sach-bao-mat">Chính sách bảo mật</a></li>
+										<li><a>Hướng dẫn trả hàng</a></li>
+										<li><a>Chính sách giao hàng</a></li>
+										<li><a>Nhận hàng hoàn trả tận nơi</a></li>
+										<li><a href="https://hoanghamobile.com/lien-he">Gửi phản hồi/Ý kiến</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="chinhanh">
+							<div class="block">
+								<div class="head">
+									<h3><i class="fa fa-map-marker"></i> Hệ thống cửa hàng</h3>
+								</div>
+								<div class="content">
+									<div class="col">
+										<h4>Khu vực Hà Nội</h4>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>194 Lê Duẩn</strong> - ĐT: 043.51.86.033</li>
+											<li><a href="https://hoanghamobile.com/194-le-duan-m1.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>382 Nguyễn Văn Cừ</strong> - ĐT: 0915.963.222</li>
+											<li><a href="https://hoanghamobile.com/382-nguyen-van-cu-m2.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>122 Thái Hà</strong> - ĐT: 097.379.0122</li>
+											<li><a href="https://hoanghamobile.com/122-thai-ha-m3.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>95B Phố Huế</strong> - ĐT: 0968.668.995</li>
+											<li><a href="https://hoanghamobile.com/95b-pho-hue-m4.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>392 Cầu giấy</strong> - ĐT: 0968.32.33.99</li>
+											<li><a href="https://hoanghamobile.com/392-cau-giay-m6.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>28 Trần phú - Hà đông</strong> - ĐT: 0911.266669</li>
+											<li><a href="https://hoanghamobile.com/28-tran-phu-ha-dong-m9.html">Bản đồ đường đi</a></li>
+										</ul>
+									</div>
+									<div class="col">
+										<h4>Khu vực TP HCM</h4>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>55 Trần Quang Khải - Quận 1</strong> - ĐT: 0942.89.22.55</li>
+											<li><a href="https://hoanghamobile.com/55-tran-quang-khai-quan-1-m5.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul>
+											<li><i class="fa fa-location-arrow"></i> <strong>1142-1144 đường 3/2 Phường 12 Quận 11</strong> - ĐT: 0932892255</li>
+											<li><a href="https://hoanghamobile.com/1142-1144-duong-32-phuong-12-quan-11-m8.html">Bản đồ đường đi</a></li>
+										</ul>
+										<ul class="single">
+											<li><i class="fa fa-location-arrow"></i> <strong>621D Cách Mạng Tháng 8, Phường 15 - Quận 10</strong> - ĐT: 0972.89.22.55</li>
+											<li><a href="https://hoanghamobile.com/621d-cach-mang-thang-8-phuong-15-quan-10-m10.html">Bản đồ đường đi</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="hoangha-info">
+							<div class="content" style="width:680px">
+								<p><strong>CÔNG TY CỔ PHẦN XÂY DỰNG VÀ ĐẦU TƯ THƯƠNG MẠI HOÀNG HÀ</strong></p>
+								<p>Địa chỉ : 95B Phố Huế, P.Ngô Thì Nhậm, Q. Hai bà trưng, Hà Nội</p>
+								<p>Mã số thuế : 0106713191. ( Đăng ký lần đầu : ngày 15 tháng 12 năm 2014, Đăng ký thay đổi ngày 09/01/2015)</p>
+							</div>
+							<a class="bct" rel="nofollow" target="_blank" href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=18190"><img alt="HoangHaMobile.Com đã đăng kí với Bộ Công Thương" title="HoangHaMobile.Com đã đăng kí với Bộ Công Thương" src="./image/logo_bct.png"></a> <a href="https://hoanghamobile.com/home/mobileswitch/?mobile=true" class="mobileVersion"><img src="./image/mobile-icon.png"><br> <span>Phiên bản Mobile</span></a>
+						</div>
+					</div>
+					<div class="right">
+						<div class="block">
+							<div class="head">
+								<h3><i class="fa fa-shopping-cart"></i> Thanh toán miễn phí</h3>
+							</div>
+							<div class="content">
+								<div class="visa"><img src="./image/visa_mastercard.gif"></div>
+							</div>
+							<div class="head">
+								<h3><i class="fa fa-truck"></i> Hình thức vận chuyển</h3>
+							</div>
+							<div class="content">
+								<div class="visa"><img src="./image/logo-vanchuyen.png"></div>
+							</div>
+						</div>
+						<div class="block">
+							<div class="head">
+								<h3><i class="fa fa-share"></i> Thông tin liên hệ</h3>
+							</div>
+							<div class="content">
+								<ul class="contact">
+									<li class="web"><a href="http://hoanghamobile.com/">http://hoanghamobile.com</a></li>
+									<li class="mail"><a href="mailto:contact@hoanghamobile.com">contact@hoanghamobile.com</a></li>
+									<li class="phone">19002091</li>
+								</ul>
+								<div class="contactIcon">
+									<ul>
+										<li class="facebook"><a href="https://www.facebook.com/hoanghamobilecom" title="HoangHaMobile Facebook Fan page">&nbsp;</a></li>
+										<li class="twitter"><a href="https://twitter.com/tuan091291" title="HoangHaMobile Twitter">&nbsp;</a></li>
+										<li class="youtube"><a href="https://plus.google.com/115378756871018292293/about" title="HoangHaMobile Youtube" target="_blank">&nbsp;</a></li>
+										<li class="googleplus"><a href="https://www.youtube.com/channel/UCJm_GdFJzT8h1odq1oMu_7Q" title="HoangHaMobile Google Plus" target="_blank">&nbsp;</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="bottom">
+				<div class="hh-container">
+					<div class="main-width">
+						<p>© Copy All rights reserved 2014 - Copyright by hoanghamobile.com</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<div id="scriptsANTS"></div>
+		<div id="p_ajax" class="modal fade" tabindex="-1" data-backdrop="static" data-keyboard="true" data-focus-on="input:first"></div>
+		<div id="p_ajax_cart" class="modal fade" tabindex="-1" data-backdrop="static" data-keyboard="true" data-focus-on="input:first"></div>
+		<div id="dialog-message" class="modal fade" tabindex="-1" data-keyboard="true"></div>
+		<div id="dialog-confirm" class="modal fade" tabindex="-1" data-backdrop="static" data-keyboard="true"></div>
+		<div id="bttop" style="display: none;"><i class="fa fa-angle-double-up"></i></div>
+		<script type="text/javascript" async src="./js/loader.js"></script>
+		<script type="text/javascript" async src="./js/atrk.js"></script>
+		<script async src="./js/gtm.js"></script>
+		<script async src="./js/analytics.js"></script>
+		<script id="facebook-jssdk" src="./js/sdk.js"></script>
+		<script type="text/javascript" async src="./js/plusone.js" gapi_processed="true"></script>
+		<script src="./js/scripts.js"></script>
+		<script async src="https://e.anthill.vn/delivery-ants/conversion.js"></script>
+		<script src="./js/app.home.min.js"></script>
+		<script src="./js/demo.js"></script>
+		<script>
+			var ants_analytic=ants_analytic||[];ants_analytic.push({conversionId:'f8c3155d'});
+		</script>
+		<script src="./js/platform.js" async defer gapi_processed="true"></script>
+		<script src="./js/conversion.js"></script>
+		<iframe name="google_conversion_frame" title="Google conversion frame" width="300" height="13" src="./js/saved_resource.html" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no"></iframe>
+		<noscript>&lt;div style=display:inline&gt;&lt;img height=1 width=1 style=border-style:none alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/961270595/?value=0&amp;amp;guid=ON&amp;amp;script=0"&gt;&lt;/div&gt;</noscript>
+		<noscript>&lt;iframe src="//www.googletagmanager.com/ns.html?id=GTM-WJ54D5" height=0 width=0 style=display:none;visibility:hidden&gt;&lt;/iframe&gt;</noscript>
+		<noscript>&lt;img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=Z1ojm1akGFL1vg" style=display:none height=1 width=1 alt=""&gt;</noscript>
+		<div class="autocomplete-suggestions" style="position: absolute; display: none; max-height: 300px; z-index: 9999;"></div>
+		<div class="autocomplete-suggestions" style="position: absolute; display: none; max-height: 300px; z-index: 9999;"></div>
+		<div style="display: none; visibility: hidden;">
+		</div>
+		<iframe id="sbzstorage_frame" src="./js/cross-storage.html" style="display: none; position: absolute; top: -999px; left: -999px;"></iframe>
+		<div class="sbzon" style="display: none; margin: 0px; padding: 0px; border: 0px; z-index: 16000001; position: fixed; bottom: 0px; overflow: hidden; right: 10px; background-color: rgba(0, 0, 0, 0);"><iframe id="sbzon_frame" frameborder="0" style="background-color: transparent; vertical-align: text-bottom; position: relative; width: 100%; height: 100%; margin: 0px; overflow: hidden;" src="javascript:false" src="./js/saved_resource(1).html"></iframe></div>
+		<div class="sbzoff" style="display: block; margin: 0px; padding: 0px; border: 0px; z-index: 16000001; position: fixed; bottom: 0px; overflow: hidden; right: 10px; width: 307.766px; height: 57px; background-color: rgba(0, 0, 0, 0);"><iframe id="sbzoff_frame" frameborder="0" style="background-color: transparent; vertical-align: text-bottom; position: relative; width: 100%; height: 100%; margin: 0px; overflow: hidden;" src="javascript:false" src="./js/saved_resource(2).html"></iframe></div>
+	</body>
+</html>
---
+++ html/header.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="en" class=" js csstransforms3d">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Điện thoại di động gía rẻ nhất - Siêu thị điện thoại Tuấn Cường Mobile</title>
+		<meta name="author" content="hoanghamobile.com">
+		<meta property="og:site_name" content="hoanghamobile.com">
+		<meta name="google-site-verification" content="JOFGGI7j9vWfBf-xpElM5Tec0UJ1k_CfdNjpaHm5z10">
+		<meta name="msvalidate.01" content="5C8CDF0992489498A30F9E5F6668A4D5">
+		<meta name="geo.placename" content="Hanoi, Hoàn Kiếm, Hanoi, Vietnam">
+		<meta name="geo.position" content="21.017249242314964;105.84134504199028">
+		<meta name="geo.region" content="VN-Hanoi">
+		<meta name="ICBM" content="21.017249242314964, 105.84134504199028">
+		<meta property="og:title" content="Điện thoại di động gía rẻ nhất - Siêu thị điện thoại Hoàng Hà Mobile">
+		<meta name="description" property="og:description" content="Điện thoại di động gía rẻ nhất - Siêu thị điện thoại Hoàng Hà Mobile">
+		<link href="./css/hoanghamobilev2.css" rel="stylesheet">
+		<script src="./js/cb=gapi.loaded_0" async></script>
+		<script async src="./js/fbevents.js"></script>
+		<script type="text/javascript" async src="./js/conversion_async.js"></script>
+		<script type="text/javascript" async src="./js/conversion_async.js"></script>
+		<script type="text/javascript" src="./js/tab_script1.js"></script>
+		<script type="text/javascript" src=".js/scripts_index.js"></script>
+		<script type="text/javascript" src="./js/tab_script1.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="./js/tab_script1.js"></script>
+	</head>
+	<body>
