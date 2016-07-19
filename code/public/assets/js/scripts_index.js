69d92c53238d1ac01aabebea002b499a47f0f492
tuancuongth88 <tuancuongth88@gmail.com>
2016-07-19 22:30:31 +0700

    update css

--- code/public/assets/js/scripts_index.js
+++ code/public/assets/js/scripts_index.js
@@ -8,7 +8,20 @@
 		style.src = "http://app_static.haithietchu.com/js/tab_script1.js" + "?_t=" + "201607167";					
 	}					
 	document.getElementsByTagName("head")[0].appendChild(style);				
-	(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://apis.google.com/js/plusone.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(po,s);})();(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/vi_VN/sdk.js#xfbml=1&appId=252305948121462&version=v2.0";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));
+	(function(){
+		var po=document.createElement('script');
+		po.type='text/javascript';po.async=true;
+		po.src='https://apis.google.com/js/plusone.js';
+		var s=document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(po,s);})();
+		(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+			if(d.getElementById(id))
+				return;
+			js=d.createElement(s);
+			js.id=id;
+		js.src="//connect.facebook.net/vi_VN/sdk.js#xfbml=1&appId=252305948121462&version=v2.0";
+		fjs.parentNode.insertBefore(js,fjs);
+	}(document,'script','facebook-jssdk'));
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-1415779-10','auto');ga('send','pageview');
 	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WJ54D5');
 	_atrk_opts={atrk_acct:"Z1ojm1akGFL1vg",domain:"hoanghamobile.com",dynamic:true};(function(){var as=document.createElement('script');as.type='text/javascript';as.async=true;as.src="https://d31qbv1cthcecs.cloudfront.net/atrk.js";var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as,s);})();
